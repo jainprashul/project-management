fb6aa464bc446353cca7b35c13d8d4304e0ba109
Prashul Jain <jainprashul@gmail.com>
2018-12-13 16:15:09 +0530

    nav links and css

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             <Route path='/project/:id' component={ProjectDetails} />
             <Route path='/signin' component={SignIn} />
             <Route path='/signup' component={SignUp} />
-            <Route path='/createproject' component={CreateProject} />
+            <Route path='/create' component={CreateProject} />
           </Switch>
 
         </div>
