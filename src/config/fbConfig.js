02db9aa667be0ff27df1cdee32efc9a7c85399a4
Prashul Jain <jainprashul@gmail.com>
2018-12-13 23:35:38 +0530

    added firebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD7ADrDj7Di0ArVB_u48U_sAJPTChWS1iA",
+    authDomain: "xpjain-marioplan.firebaseapp.com",
+    databaseURL: "https://xpjain-marioplan.firebaseio.com",
+    projectId: "xpjain-marioplan",
+    storageBucket: "xpjain-marioplan.appspot.com",
+    messagingSenderId: "249885021827"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().setting({ timestampsInSnapshots: true })
+
+export default firebase
