3b0e25111d153141d580381062db7a23540bd62b
Prashul Jain <jainprashul@gmail.com>
2018-12-13 17:02:19 +0530

    connected redux with react components and added dummy data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects : [
+        {id: '1', title:'Collect all eggs', content:'lol lol lol'},
+        {id: '2', title:'Burn all eggs', content:'lol lol lol'},
+        {id: '3', title:'Collect all Sabes', content:'lol lol lol'},
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state;
