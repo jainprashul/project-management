226b81e3da0cdcbf843d89162f62f8a316ff68aa
Prashul Jain <jainprashul@gmail.com>
2018-12-14 15:00:42 +0530

    added login feature

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,25 @@
-const initState = {}
+const initState = {
+    authError: null
+}
 
 const authReducer = (state = initState, action) => {
-    return state;
+    switch(action.type){
+        case 'LOGIN_ERROR':
+            console.log(action.err)
+            return {
+                ...state,
+                authError : action.err.message
+            };
+        
+        case 'LOGIN_SUCCESS':
+            console.log('login success')
+            return {
+                ...state,
+                authError : null
+            }
+        default:
+            return state;
+    }
 }
 
 export default authReducer
