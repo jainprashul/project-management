875fd254e14a1c70a921635f911171f4c098461c
Prashul Jain <jainprashul@gmail.com>
2018-12-14 15:56:42 +0530

    waiting for auth ready to prevent flikering

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware, compose} from 'redux'
-import {Provider} from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
 import rootReducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk'
 import { getFirestore, reduxFirestore } from "redux-firestore";
@@ -15,17 +15,19 @@ const store = createStore(rootReducer,
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
         reduxFirestore(fbConfig),
-        reactReduxFirebase(fbConfig)
-        )
-    ); 
+        reactReduxFirebase(fbConfig, { attachAuthIsReady: true })
+    )
+);
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+        , document.getElementById('root'));
+    serviceWorker.register();
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-    , document.getElementById('root'));
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
