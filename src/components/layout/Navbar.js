a572ed56bdb5372908039516744e987580e0c153
Prashul Jain <jainprashul@gmail.com>
2018-12-15 02:33:33 +0530

    user profile badge

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,9 +6,9 @@ import { firebaseConnect } from 'react-redux-firebase';
 import { connect } from "react-redux";
 
 const Navbar = (props) =>{
-    const { auth } = props;
+    const { auth, profile } = props;
     //console.log(auth)
-    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+    const links = auth.uid ? <SignedInLinks profile={profile}  /> : <SignedOutLinks />
     return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
@@ -23,7 +23,8 @@ const Navbar = (props) =>{
 const mapStateToProps = (state) => {
     //console.log(state);
     return{
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        profile:  state.firebase.profile
     }
 }
 
