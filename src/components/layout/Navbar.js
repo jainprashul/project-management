afcdd07f51f102fa1cefd7bc6f0dd8c72cb8d170
Prashul Jain <jainprashul@gmail.com>
2018-12-14 15:52:27 +0530

    tracking auth status  and  navbar links for signin and signout

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,13 +7,13 @@ import { connect } from "react-redux";
 
 const Navbar = (props) =>{
     const { auth } = props;
-    console.log(props)
+    //console.log(auth)
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
     return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className='brand-logo'>MarioPlan</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                {links}
 
             </div>
         </nav>
