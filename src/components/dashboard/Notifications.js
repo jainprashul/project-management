a70e7fa69ab8148bae7a2dc3aec30c736af70cfd
Prashul Jain <jainprashul@gmail.com>
2018-12-20 21:51:57 +0530

    notification component and configuring cloud functions

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const Notifications = () => {
-    return(
-        <div>
-            <p>Notifications</p>
+const Notifications = (props) => {
+    return (
+        <div className='section'>
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Notifications</span>
+                    <ul className='notifications'>
+                        <li>Notifications</li>
+                        <li>Notifications</li>
+                        <li>Notifications</li>
+                        <li>Notifications</li>
+
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
