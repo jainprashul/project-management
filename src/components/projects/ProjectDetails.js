cf05b6c634e36ff947e886747da7023b822fb7e3
Prashul Jain <jainprashul@gmail.com>
2018-12-20 20:58:30 +0530

    added date format with moment js

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,35 +1,36 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
 import { Redirect } from "react-router-dom";
+import { compose } from 'redux';
+import moment from 'moment'
 
 
 const ProjectDetails = (props) => {
    const { project, auth } = props;
-   if(!auth.uid) return <Redirect to='/signin' />
+   if (!auth.uid) return <Redirect to='/signin' />
    if (project) {
-      return(
+      return (
          <div className='container section project-details'>
-         <div className="card z-depth-0">
-            <div className="card-content">
-               <span className="card-title">{ project.title }</span>
-               <p>{ project.content }</p>
-               <div className="card-action gret lighten-4 grey-text">
-                  <div>Posted By {project.authorFirstName} {project.authorLastName}</div>
-                  <div>23 December , 4am</div>
+            <div className="card z-depth-0">
+               <div className="card-content">
+                  <span className="card-title">{project.title}</span>
+                  <p>{project.content}</p>
+                  <div className="card-action gret lighten-4 grey-text">
+                     <div>Posted By {project.authorFirstName} {project.authorLastName}</div>
+                     <div>{moment(project.createdAt.toDate().toString()).calendar()}</div>
+                  </div>
                </div>
             </div>
          </div>
-      </div>
       )
    } else {
-      return(
+      return (
          <div className='container center'>
             <p>Loading project...</p>
          </div>
       )
-  
+
    }
 }
 
